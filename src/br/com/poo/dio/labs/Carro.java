319ac0570e17d65211a349f4beb05e0ee2e33fe1
aevilesaguiar <aeviles@hotmail.com>
2022-01-20 15:44:40 -0300

    iniciando bootcamp programação orientada a Objetos

---
+++ src/br/com/poo/dio/labs/Carro.java
@@ -0,0 +1,13 @@
+package br.com.poo.dio.labs;
+
+import java.math.BigDecimal;
+
+public class Carro {
+	
+	private String cor;
+	private String placa;
+	private BigDecimal preco;
+	private double capacidadeTanque;
+	
+
+}
